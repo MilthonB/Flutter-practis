b0b13b2f3802eb37bd8d3c1db2ed7bd33da110a1
EMEBE <81788252+MilthonB@users.noreply.github.com>
2024-10-27 11:12:08 -0600

    Se termino el welcome

--- 01-login/lib/main.dart
+++ 01-login/lib/main.dart
@@ -3,7 +3,7 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_fonts/google_fonts.dart';
-
+import 'package:animate_do/animate_do.dart';
 
 void main() {
   runApp(const MainApp());
@@ -14,28 +14,23 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-        scaffoldBackgroundColor: Color.fromARGB(255, 107, 244, 254)
-      ),
+          primarySwatch: Colors.blue,
+          scaffoldBackgroundColor: Color.fromARGB(255, 107, 244, 254)),
       home: HomeScreen(),
     );
   }
 }
 
-
 class HomeScreen extends StatefulWidget {
-
-  
-
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  List<String> svgList =<String>[
+  List<String> svgList = <String>[
     'assets/images/login/initial2.svg',
     'assets/images/login/initial3.svg',
     'assets/images/login/initial4.svg',
@@ -43,28 +38,39 @@ class _HomeScreenState extends State<HomeScreen> {
     'assets/images/login/initial6.svg'
   ];
 
-  late PageController  _pageController;
+  List<String> textList = <String>[
+    'Finding',
+    'Revelation',
+    'Uncovering',
+    'Detection',
+    'Discover'
+  ];
+
+  late PageController _pageController;
   int _currentIndex = 0;
 
   @override
   void initState() {
     super.initState();
-    _pageController = PageController();
+    _pageController = PageController(initialPage: svgList.length);
 
     Timer.periodic(const Duration(seconds: 3), (timer) {
-      if(_currentIndex < svgList.length -1 ){
+      if (_currentIndex < svgList.length - 1) {
         _currentIndex++;
-      }else {
+      } else {
+        setState(() {});
         _currentIndex = 0;
-        FadeInLeft()
-        // _pageController.jumpTo(0);
-        // _pageController.animateTo(1, duration: const Duration(microseconds: 300), curve: Curves.easeInOut);
-        // svgList.removeAt(0);
       }
-      _pageController.animateToPage(_currentIndex, duration: const Duration(milliseconds: 300), curve: Curves.easeInOut);
-    },);
+      // print(_currentIndex);
+      _pageController.jumpToPage(_currentIndex);
+      // _pageController.jumpTo(_currentIndex+0);
+      // _pageController.animateToPage(
+      //   _currentIndex,
+      //   duration: const Duration(milliseconds: 300),
+      //   curve: Curves.bounceOut,
+      // );
+    });
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -82,53 +88,74 @@ class _HomeScreenState extends State<HomeScreen> {
         ),
         child: Column(
           children: [
-            const SizedBox(height: 150,),
+            const SizedBox(
+              height: 150,
+            ),
             Center(
               child: Text(
                 'Welcome',
                 style: GoogleFonts.redHatDisplay(
-                  color: Colors.blue,
-                  fontSize: 35,
-                  fontWeight: FontWeight.w800
-                ),
+                    color: Colors.blue,
+                    fontSize: 35,
+                    fontWeight: FontWeight.w800),
               ),
             ),
-            const SizedBox(height: 60,),
+            const SizedBox(
+              height: 60,
+            ),
             SizedBox(
-              height: 210,
-              child: PageView.builder(
-                controller: _pageController,
-                itemCount: svgList.length,
-                itemBuilder: (context, index) {
-                  return Center(
-                    child: SvgPicture.asset(svgList[index]),
-                  );
-                },
-              ) 
+                height: 360,
+                child: PageView.builder(
+                  controller: _pageController,
+                  itemCount: svgList.length,
+                  itemBuilder: (context, index) {
+                    return Center(
+                        // child: SvgPicture.asset(svgList[index])
+                        child: FadeInRight(
+                            animate: true,
+                            curve: Curves.linear,
+                            duration: const Duration(seconds: 1),
+                            child: Column(
+                              children: [
+                                SizedBox(
+                                  height: 170,
+                                  child: SvgPicture.asset(svgList[index]),
+                                ),
+                                SizedBox(
+                                  height: 20,
+                                ),
+                                const SizedBox(
+                                  height: 50,
+                                ),
+                                Center(
+                                  child: Text(
+                                    '${textList[index]} you',
+                                    style: GoogleFonts.redHatDisplay(
+                                        color: Colors.black87, fontSize: 35),
+                                  ),
+                                ),
+                                Center(
+                                  child: Text(
+                                    'Dreams job here',
+                                    style: GoogleFonts.redHatDisplay(
+                                        color: Colors.blue,
+                                        fontSize: 35,
+                                        fontWeight: FontWeight.w800),
+                                  ),
+                                ),
+                              ],
+                            ))
+                        // child:  FadeIn(
+                        //   animate: true,
+                        //   curve: Curves.easeOut,
+                        //   child: SvgPicture.asset(svgList[index])
+                        //   )  ,
+                        );
+                  },
+                )),
+            const SizedBox(
+              height: 30,
             ),
-            const SizedBox(height: 50,),
-            Center(
-              child: Text(
-                'Discover you',
-                style: GoogleFonts.redHatDisplay(
-                  color: Colors.black87,
-                  fontSize: 35
-                ),
-              ),
-            ),
-            Center(
-              child: Text(
-                'Dreams job here',
-                style: GoogleFonts.redHatDisplay(
-                  color: Colors.blue,
-                  fontSize: 35,
-                  fontWeight: FontWeight.w800
-                ),
-              ),
-            ),
-            
-            const SizedBox(height: 30,),
-
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 30.0),
               child: Center(
@@ -136,43 +163,52 @@ class _HomeScreenState extends State<HomeScreen> {
                   'Explore all the most existing jobs roles based on your interesent and study major ',
                   textAlign: TextAlign.center,
                   style: GoogleFonts.redHatDisplay(
-                    color: Colors.black45,
-                    fontSize: 17,
-                    fontWeight: FontWeight.w800
-                  ),
+                      color: Colors.black45,
+                      fontSize: 17,
+                      fontWeight: FontWeight.w800),
                 ),
               ),
             ),
-            const SizedBox(height: 30,),
-
+            const SizedBox(
+              height: 30,
+            ),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  minimumSize: const Size(130, 50),
-                  backgroundColor: Colors.blue,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10.0)
-                  )
+                ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        minimumSize: const Size(130, 50),
+                        backgroundColor: Colors.blue,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10.0))),
+                    onPressed: () {},
+                    child: Text(
+                      'Register',
+                      style: GoogleFonts.redHatDisplay(
+                          fontSize: 15,
+                          fontWeight: FontWeight.w700,
+                          color: Colors.white),
+                    )),
+                const SizedBox(
+                  width: 10,
                 ),
-                onPressed: () {
-                
-              }, child: Text('Register', style: GoogleFonts.redHatDisplay(fontSize: 15, fontWeight: FontWeight.w700, color: Colors.white),)),
-
-              const SizedBox(width: 10,),
-              
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  minimumSize: const Size(130, 50),
-                  backgroundColor: const Color.fromARGB(255, 236, 236, 236),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10.0)
-                  )
-                ),
-                onPressed: () {
-              }, child: Text('Sing In', style: GoogleFonts.redHatDisplay( fontSize: 15, fontWeight: FontWeight.w700, color: Colors.black54 ),))
-            ],)
+                ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        minimumSize: const Size(130, 50),
+                        backgroundColor:
+                            const Color.fromARGB(255, 236, 236, 236),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10.0))),
+                    onPressed: () {},
+                    child: Text(
+                      'Sing In',
+                      style: GoogleFonts.redHatDisplay(
+                          fontSize: 15,
+                          fontWeight: FontWeight.w700,
+                          color: Colors.black54),
+                    ))
+              ],
+            )
           ],
         ),
       ),
