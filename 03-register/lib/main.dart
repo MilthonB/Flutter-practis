25f4ae7e1457b438b5ca4f5f9c059492e1157548
EMEBE <81788252+MilthonB@users.noreply.github.com>
2024-10-28 15:18:18 -0600

    Finish register screen

--- 03-register/lib/main.dart
+++ 03-register/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,43 +12,15 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -55,71 +29,164 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 45.0),
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(height: 150,),
+                Text(
+                  'Create Account',
+                  style: GoogleFonts.redHatDisplay(
+                      color: Colors.blue,
+                      fontWeight: FontWeight.w900,
+                      fontSize: 25),
+                ),
+                Text(
+                  'Create on account so you can  explore all the existing jobs',
+                  style: GoogleFonts.redHatDisplay(
+                      color: Colors.black38,
+                      fontWeight: FontWeight.w500,
+                      fontSize: 15),
+                ),
+                SizedBox(height: 20,),
+                TextFormField(
+                  autocorrect: false,
+                  cursorColor: Colors.black,
+                  decoration: InputDecoration(
+                    hintText: 'Enter email',
+                    hintStyle: GoogleFonts.redHatDisplay(
+                        fontWeight: FontWeight.w100, color: Colors.black38),
+                    enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(15.0),
+                        borderSide:
+                            const BorderSide(color: Colors.black12, width: 1.0)),
+                    focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(15.0),
+                        borderSide:
+                            const BorderSide(color: Colors.black12, width: 1.0)),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(15),
+                        borderSide: const BorderSide(style: BorderStyle.solid)),
+                  ),
+                ),
+                SizedBox(height: 20,),
+          
+                TextFormField(
+                  autocorrect: false,
+                  obscureText: true,
+                  cursorColor: Colors.black,
+                  decoration: InputDecoration(
+                    hintText: 'Password',
+                    hintStyle: GoogleFonts.redHatDisplay(
+                        fontWeight: FontWeight.w100, color: Colors.black38),
+                    enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(15.0),
+                        borderSide:
+                            const BorderSide(color: Colors.black12, width: 1.0)),
+                    focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(15.0),
+                        borderSide:
+                            const BorderSide(color: Colors.black12, width: 1.0)),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(15),
+                        borderSide: const BorderSide(style: BorderStyle.solid)),
+                  ),
+                ),
+                SizedBox(height: 20,),
+          
+                TextFormField(
+                  obscureText: true,
+                  autocorrect: false,
+                  cursorColor: Colors.black,
+                  decoration: InputDecoration(
+                    hintText: 'Password confirm',
+                    
+                    hintStyle: GoogleFonts.redHatDisplay(
+                        fontWeight: FontWeight.w100, color: Colors.black38),
+                    enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(15.0),
+                        borderSide:
+                            const BorderSide(color: Colors.black12, width: 1.0)),
+                    focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(15.0),
+                        borderSide:
+                            const BorderSide(color: Colors.black12, width: 1.0)),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(15),
+                        borderSide: const BorderSide(style: BorderStyle.solid)),
+                  ),
+                ),
+                
+                const SizedBox(height: 20,),
+                SizedBox(
+                  width: double.infinity,
+                  height: 60.0,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom (
+                      backgroundColor:Colors.blue,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15.0)
+                      )
+                    ),
+                    onPressed: () {
+                  }, child: Text('Sing in', style: GoogleFonts.redHatDisplay( color: Colors.white, fontWeight: FontWeight.w800, fontSize: 18),)),
+                ),
+          
+                const SizedBox(height: 15,),
+                // Spacer(),
+                Align(
+                  alignment: Alignment.bottomCenter,
+                  child: TextButton(onPressed:() {                  
+                  }, child: Text('Already have a account', style: GoogleFonts.redHatDisplay(fontWeight: FontWeight.w700, color: Colors.black45),)),
+                ),
+          
+                const SizedBox(height: 30,),
+                    Row(
+                      children: [
+                        const Expanded(child: Divider(
+                          color: Colors.black12,
+                          thickness: 1,
+                        ),),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text('Or continue with', style: GoogleFonts.redHatDisplay(),),
+                        ),
+                        const Expanded(child: Divider(
+                          color: Colors.black12,
+                          thickness: 1,
+                        ),),
+                      ],
+                    ),
+                    const SizedBox(height: 15,),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                      IconButton(onPressed: () {
+                        
+                      }, icon: const FaIcon(FontAwesomeIcons.facebook,size: 35, color: Colors.blue,),), 
+                      // const Icon(Icons.apple, size: 35,),),
+                      // Spacer(),
+                      IconButton(onPressed: () {
+                        
+                      }, icon: const FaIcon(FontAwesomeIcons.google, size: 35, color: Colors.black54,),), 
+                      //const Icon(Icons.facebook, size: 35, color: Colors.blue,),),
+                      // Spacer(),
+                      IconButton(onPressed: () {
+                        
+                      }, icon: const FaIcon(FontAwesomeIcons.github, size: 35, color: Colors.blueGrey,),), 
+                      //icon: const Icon(Icons.email, size: 35,),),
+                      // Spacer(),
+                      ],
+                    ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+          ),
+        ));
   }
 }
