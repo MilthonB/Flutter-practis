f51f8637a324e093f243c3d19b1a6c1406e723f2
EMEBE <81788252+MilthonB@users.noreply.github.com>
2024-12-21 19:44:47 -0600

    Animationcontainer

--- 09-animacioncontainer/lib/main.dart
+++ 09-animacioncontainer/lib/main.dart
@@ -1,18 +1,74 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+
+  double _w = 0;
+  double _h = 15;
+
+  // changeSize(){
+  //   Random ran = Random();
+  //   w = ran.nextDouble(  ) * 250; 
+  //   h = ran.nextDouble(  ) * 100;
+  //   setState(() {
+      
+  //   });
+  // }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    // _w = 50;
+    //       _h = 50;
+    Future.delayed(
+      Duration(milliseconds: 500), () {
+        setState(() {
+          _w = 30;
+          // _h = 15;    
+        });
+      },
+    );
+
+
+  }
+
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    
+    return  MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: AnimatedContainer(
+            duration: Duration(milliseconds: 500),
+            curve: Curves.easeIn,
+            width: _w,
+            height: _h,
+            decoration: BoxDecoration(
+              color: Colors.red,
+              borderRadius: BorderRadius.circular(10)
+            ),
+          ),
+        ) ,
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+          },
+          child: Icon(Icons.add, color: Colors.white,),
+          backgroundColor: Colors.teal,
         ),
       ),
     );
